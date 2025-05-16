acdfc87a9e7f2784dbf310b73d4a2452d40f52c3
Raishsh <raitobias777@gmail.com>
2025-05-16 14:13:23 -0300

    Criando entidade no banco de dados a partir do PessoasModel

---
+++ src/main/java/dev/rai/Cadastro/PessoasModel.java
@@ -0,0 +1,52 @@
+package dev.rai.Cadastro;
+
+
+import jakarta.persistence.*;
+
+
+//Entity transforma uma classe comum numa entidade do banco de dados.
+
+@Entity
+@Table(name = "tb_cadastro")
+public class PessoasModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+
+    public PessoasModel() {
+    }
+
+    public PessoasModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
