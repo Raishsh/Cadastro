b7d1839c135cedb69f98e959a6a4be008d5853fd
Raishsh <raitobias777@gmail.com>
2025-05-20 01:14:55 -0300

    feat: Lombok added

--- src/main/java/dev/rai/Cadastro/Empregos/EmpregoModel.java
+++ src/main/java/dev/rai/Cadastro/Empregos/EmpregoModel.java
@@ -2,42 +2,30 @@ package dev.rai.Cadastro.Empregos;
 
 import dev.rai.Cadastro.Pessoas.PessoasModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table (name = "tb_emprego")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class EmpregoModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String emprego;
+
     private double salario;
+
     @OneToMany(mappedBy = "missoes")
     private List<PessoasModel> pessoa;
 
-    public EmpregoModel() {
-    }
 
-    public EmpregoModel(String emprego, double salario) {
-        this.emprego = emprego;
-        this.salario = salario;
-    }
-
-    public String getEmprego() {
-        return emprego;
-    }
-
-    public void setEmprego(String emprego) {
-        this.emprego = emprego;
-    }
-
-    public double getSalario() {
-        return salario;
-    }
-
-    public void setSalario(double salario) {
-        this.salario = salario;
-    }
 }
--- src/main/java/dev/rai/Cadastro/Pessoas/PessoasModel.java
+++ src/main/java/dev/rai/Cadastro/Pessoas/PessoasModel.java
@@ -3,6 +3,9 @@ package dev.rai.Cadastro.Pessoas;
 
 import dev.rai.Cadastro.Empregos.EmpregoModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
@@ -11,48 +14,23 @@ import java.util.List;
 
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PessoasModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
+
     private String email;
+
     private int idade;
+
     @ManyToOne
     @JoinColumn(name = "emprego_id")
     private EmpregoModel emprego;
 
-    public PessoasModel() {
-    }
-
-    public PessoasModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
